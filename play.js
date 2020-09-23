8000f39813b53a48c281083d948f1fc99a00c35b
csjohn1 <csjohn@shaw.ca>
2020-09-23 17:03:17 +0000

    adding initial client

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.0.139',
+    port: 50542
+  });
+
+  conn.setEncoding('utf8');
+
+  // conn.on('data', (data) => {
+  //   console.log('Server says: ', data);
+  // });
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
