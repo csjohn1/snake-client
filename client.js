05c781c3eb418d14c03ff1d8a2cf662784bf04c1
csjohn1 <csjohn@shaw.ca>
2020-09-23 18:24:23 +0000

    adding movement

--- client.js
+++ client.js
@@ -2,17 +2,27 @@ const net = require('net');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '192.168.0.139',
-    port: 50542
+    //host: '192.168.0.139',
+    host: '10.0.2.15',
+    // port: 50542
+    port: 50541
   });
 
   conn.setEncoding('utf8');
 
-  // conn.on('data', (data) => {
-  //   console.log('Server says: ', data);
-  // });
+  conn.on('connect', () => {
+    console.log('connected to server');
+    conn.write('Name: CSJ');
+    // conn.write('Move: up');
+
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
 
   return conn;
 }
+
 
 module.exports = connect;
