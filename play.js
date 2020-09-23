febfffb34efffe2b00df2893a56f856b5d851715
csjohn1 <csjohn@shaw.ca>
2020-09-23 17:15:03 +0000

    moved code to client file and exported to play

--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require('net');
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.0.139',
-    port: 50542
-  });
-
-  conn.setEncoding('utf8');
-
-  // conn.on('data', (data) => {
-  //   console.log('Server says: ', data);
-  // });
-
-  return conn;
-}
-
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
